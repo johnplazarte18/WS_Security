36236abfa96290a662933551fd4334b794f1c584
CarlosAlmeida2000 <carlos.almeida2017@uteq.edu.ec>
2021-08-08 11:06:09 -0500

    Método GET para obtener el historial de anomalías en un 90 % de terminado

--- seguridad/views.py
+++ seguridad/views.py
@@ -1,10 +1,32 @@
-from typing import FrozenSet
-from django.http import response
+from json.decoder import JSONDecodeError
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from seguridad.models import historial, componentes, evidencias
+from django.http import JsonResponse
+import json
 
 class Anomalia(APIView):
-    def get(self, request, format=None):
-        return Response({"mensaje": "holaa get"})
-    def post(self, request, format=None):
-        return Response({"mensaje": "holaa post"})
+    def get(self, request, format = None):
+        json_historial = list()
+        for h in historial.objects.all():    
+            json_evidencias = list()
+            for e in evidencias.objects.filter(unHistorial_id=h.id):
+                evidencia = {
+                    "evidencia_id": e.id,
+                    "hora": str(e.hora),
+                    "foto": "e.ruta_foto"
+                }
+                json_evidencias.append(evidencia)
+            anomalia = {
+                "historial_id" : h.id,
+                "componente_id": h.unComponente.id,
+                "componente_nombre": h.unComponente.nombre,
+                "tipo_historial": h.tipo,
+                "evidencias": json_evidencias
+            }
+            json_historial.append(anomalia)
+        return Response({"historial": json_historial})
+
+    def post(self, request, format = None):
+        json_data = json.loads(request.body)
+        return Response({"mensaje": json_data})
